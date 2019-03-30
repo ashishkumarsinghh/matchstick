adc7b3810ca82bb190c08612c2eb82b3c9cab0ec
ashish kumar singh <ashishkumarsinghh@outlook.com>
2019-03-30 23:54:32 +0900

    Setup EsLint

--- server/scripts/randomUsers.js
+++ server/scripts/randomUsers.js
@@ -1,28 +1,24 @@
-const fetch = require("node-fetch");
-const fs = require("fs");
+import fetch from 'node-fetch';
+import { writeFile } from 'fs';
 
-fetch("http://randomuser.me/api/?results=2000&gender=female&nat=us")
+fetch('http://randomuser.me/api/?results=2000&gender=female&nat=us')
   .then(res => res.json())
-  .then(data =>
-    fs.writeFile("./data/females.txt", JSON.stringify(data), err => {
-      if (err) {
-        console.log("error writing female data to file.");
-      }
+  .then(data => writeFile('./data/females.txt', JSON.stringify(data), (err) => {
+    if (err) {
+      console.log('error writing female data to file.');
+    }
 
-      console.log("Female data written.");
-    })
-  );
+    console.log('Female data written.');
+  }));
 
-fetch("http://randomuser.me/api/?results=2000&gender=male&nat=us")
+fetch('http://randomuser.me/api/?results=2000&gender=male&nat=us')
   .then(res => res.json())
-  .then(data =>
-    fs.writeFile("./data/males.txt", JSON.stringify(data), err => {
-      if (err) {
-        console.log("error writing male data to file.");
-      }
-      console.log("Male data written.");
-    })
-  );
+  .then(data => writeFile('./data/males.txt', JSON.stringify(data), (err) => {
+    if (err) {
+      console.log('error writing male data to file.');
+    }
+    console.log('Male data written.');
+  }));
 
-//Mongodb Import Script to import data from json array stored in file
+// Mongodb Import Script to import data from json array stored in file
 // mongoimport --db test --collection people --file males.txt --jsonArray
